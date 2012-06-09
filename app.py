6bd15ab8ceb43595753c08d472b82159a1fd3cb8
Danilo Bargen <gezuru@gmail.com>
2012-06-09 17:32:40 +0200

    Load debug configuration from env variable

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from pymongo import Connection
 app = Flask(__name__)
 heroku = Heroku(app)
 
-app.debug = True
-
 connection = Connection(app.config['MONGODB_HOST'], app.config['MONGODB_PORT'])
 db = connection[app.config['MONGODB_DB']]
 
@@ -65,4 +63,5 @@ def headline():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    debug = os.environ.get('DEBUG') in ['true', 'True', 'TRUE', 't', 'T', '1']
+    app.run(host='0.0.0.0', port=port, debug=debug)
