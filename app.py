f3257de781c87a2320d571d9036f1606cf3ffda9
Danilo Bargen <gezuru@gmail.com>
2012-06-09 17:26:25 +0200

    Headline generation works!

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
+from random import randrange
 
 from flask import Flask
 from flask_heroku import Heroku
-from  pymongo import Connection
+from pymongo import Connection
 
 
 app = Flask(__name__)
@@ -14,11 +15,50 @@ connection = Connection(app.config['MONGODB_HOST'], app.config['MONGODB_PORT'])
 db = connection[app.config['MONGODB_DB']]
 
 
+### Helper functions ###
+
+def mongo_get_random(collection):
+    collection = db[collection]
+    count = collection.count()
+    offset = randrange(1, count)
+    return collection.find().skip(offset).limit(1)[0]
+
+def generate_headline():
+    """Generate and return an awesome headline."""
+
+    # Correct endings
+    adjective_endings = {
+        'm': 'r',
+        'w': '',
+        's': 's',
+        'p': '',
+    }
+
+    # Get random database entries
+    d_intro = mongo_get_random('intro')
+    d_adjective = mongo_get_random('adjective')
+    d_prefix = mongo_get_random('prefix')
+    d_suffix = mongo_get_random('suffix')
+    d_action = mongo_get_random('action')
+
+    case = d_suffix['case']
+    intro = d_intro['text']
+    adjective = d_adjective['text'] + adjective_endings[case]
+    prefix = d_prefix['text']
+    suffix = d_suffix['text']
+    if case == 'p':
+        action = '%s %s' % (d_action['action_p'], d_action['text'])
+    else:
+        action = '%s %s' % (d_action['action_s'], d_action['text'])
+
+    return '%s: %s %s-%s %s!' % (intro, adjective, prefix, suffix, action)
+
+
 ### Views ###
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def headline():
+    return generate_headline()
 
 
 ### Dev Server ###
