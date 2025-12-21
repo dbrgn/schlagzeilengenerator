6ea554e946d5f99fab5b9da0796c2cb879ee9cb8
Danilo Bargen <mail@dbrgn.ch>
2025-12-21 14:25:42 +0100

    nix: Simplify package

--- nix/package.nix
+++ nix/package.nix
@@ -1,71 +1,48 @@
 {
+  stdenv,
   lib,
   python3,
-  python3Packages,
-}:
-python3Packages.buildPythonApplication rec {
-  pname = "schlagzeilengenerator";
-  version = "1.0.0";
-  format = "pyproject";
+  makeWrapper,
+}: let
+  python = python3.withPackages (packages: [
+    packages.flask
+    packages.gunicorn
+  ]);
+in
+  stdenv.mkDerivation {
+    pname = "schlagzeilengenerator";
+    version = "1.0.0";
 
-  src = lib.cleanSourceWith {
-    src = ../.;
-    filter = path: type: let
-      baseName = baseNameOf path;
-      relPath = lib.removePrefix "${toString ../.}/" path;
-    in
-      # Include app/ directory and its contents
-      lib.hasPrefix "app/" relPath
-      ||
-      # Include specific root files
-      builtins.elem baseName ["README.md" "LICENSE"];
-  };
+    src = ../app;
 
-  # Point to the app subdirectory for Python package
-  sourceRoot = "source/app";
+    nativeBuildInputs = [makeWrapper];
 
-  nativeBuildInputs = with python3Packages; [
-    setuptools
-  ];
+    installPhase = ''
+      runHook preInstall
 
-  propagatedBuildInputs = with python3Packages; [
-    flask
-    gunicorn
-  ];
+      # Copy application files
+      mkdir -p $out/share/schlagzeilengenerator
+      cp -r . $out/share/schlagzeilengenerator/
 
-  # Don't run tests (there are none)
-  doCheck = false;
+      # Create wrapper script
+      mkdir -p $out/bin
+      makeWrapper ${python}/bin/gunicorn $out/bin/schlagzeilengenerator \
+        --chdir $out/share/schlagzeilengenerator \
+        --add-flags "--bind \"\''${SCHLAGZEILEN_HOST:-127.0.0.1}:\''${SCHLAGZEILEN_PORT:-8000}\"" \
+        --add-flags "--workers 2" \
+        --add-flags "--threads 2" \
+        --add-flags "--access-logfile -" \
+        --add-flags "--error-logfile -" \
+        --add-flags "app:app"
 
-  # Install additional data files
-  postInstall = ''
-        # Copy data files
-        mkdir -p $out/share/schlagzeilengenerator
-        cp -r data $out/share/schlagzeilengenerator/
-        cp -r static $out/share/schlagzeilengenerator/
-        cp -r templates $out/share/schlagzeilengenerator/
+      runHook postInstall
+    '';
 
-        # Create wrapper script
-        mkdir -p $out/bin
-        cat > $out/bin/schlagzeilengenerator <<EOF
-    #!/bin/sh
-    cd $out/share/schlagzeilengenerator
-    export PYTHONPATH="$out/${python3.sitePackages}:\$PYTHONPATH"
-    exec ${python3Packages.gunicorn}/bin/gunicorn \\
-      --bind "\''${SCHLAGZEILEN_HOST:-127.0.0.1}:\''${SCHLAGZEILEN_PORT:-8000}" \\
-      --workers 2 \\
-      --threads 2 \\
-      --access-logfile - \\
-      --error-logfile - \\
-      app:app "\$@"
-    EOF
-        chmod +x $out/bin/schlagzeilengenerator
-  '';
-
-  meta = with lib; {
-    description = "A small web application to generate tabloid press headlines";
-    homepage = "https://schlagzeilengenerator.ch";
-    license = licenses.bsd3;
-    maintainers = [];
-    mainProgram = "schlagzeilengenerator";
-  };
-}
+    meta = with lib; {
+      description = "A small web application to generate tabloid press headlines";
+      homepage = "https://schlagzeilengenerator.ch";
+      license = licenses.bsd3;
+      maintainers = [];
+      mainProgram = "schlagzeilengenerator";
+    };
+  }
